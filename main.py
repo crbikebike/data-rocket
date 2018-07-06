13f1c0c166844a8fa96a15efdaa3fd3b8da86978
Chris Race <chris.race@revunit.com>
2018-07-06 09:19:02 -0700

    added comments and clarity

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import controllers.ormcontroller as orm
 ### Main Function - All the work facilitated from here.
 if __name__ == '__main__':
 
-    # Instantiate our friendly data Munger
+    # Instantiate our friendly data Munger to ready data
     mungy = Munger()
 
     # Place Munged Harvest entries into a dictionary
@@ -15,27 +15,23 @@ if __name__ == '__main__':
     tasks = mungy.munge_task_list()
     projects = mungy.munge_project_list()
 
+    # Insert each entity
     print('Inserting People DB records')
-    # Insert people
     people_list = people['users']
     orm.insert_people_list(people_list)
 
     print('Inserting Client DB records')
-    # Insert clients
     client_list = clients['clients']
     orm.insert_clients_list(client_list)
 
     print('Inserting Project DB records')
-    # Insert projects
     project_list = projects['projects']
     orm.insert_projects_list(project_list)
 
     print('Inserting Task DB records')
-    # Insert tasks
     tasks_list = tasks['tasks']
     orm.insert_tasks_list(tasks_list)
 
     print('Inserting Time Entry DB records')
-    # Get the list of time entries to insert, send them to the DataActor
     time_entry_list = time_entries['time_entries']
     orm.insert_time_entries_list(time_entry_list)
