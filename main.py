81381e376cb61df13fcba52dc2a6df14c4404b2d
Chris Race <chris.race@revunit.com>
2018-07-15 19:46:19 -0700

    Also fixed bug for time_entry run

--- main.py
+++ main.py
@@ -19,7 +19,7 @@ if __name__ == '__main__':
     # Load all data flagged as true and push to db
     pb.load_data(full_load=config['full_load'], all_tables=config['all_tables'], people=config['people'], clients=config['clients'],
                  tasks=config['tasks'], projects=config['projects'], assignments=config['assignments'],
-                 time_entries=['time_entries'])
+                 time_entries=config['time_entries'])
 
     # Write to completion log
     logger.write_load_completion(argv[1:], success=True)
