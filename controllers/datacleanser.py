4ae0d77580f42b40212b3de6dae307043ad7ccea
Chris Race <chris.race@revunit.com>
2018-07-18 00:12:41 -0700

    POC for Pandas powering the diff is done.

--- controllers/datacleanser.py
+++ controllers/datacleanser.py
@@ -25,12 +25,26 @@ class GarbageCollector(object):
         harvest_people = self.harv.get_harvest_users(updated_since=full_load_datetime)['users']
         forecast_people = self.fore.get_forecast_people()['people']
 
-        # Combine Forecast and Harvest people
-        for f_person in forecast_people:
-            pass
+        # Combine Forecast and Harvest people using Pandas Data Frames
+        hdf = pd.DataFrame(harvest_people)
+        fdf = pd.DataFrame(forecast_people)
+        combined = fdf.merge(hdf, how='outer', on='hid', suffixes=('_fore', '_harv'))
 
-        # Make sets of pks from all three lists
-        dw_people = {(p.harvest_id, p.forecast_id) for p in people_list}
-        h_people =  {p['id'] for p in harvest_people}
-        f_people = {p['id'] for p in forecast_people}
+        # Convert the combined DataFrame back into a list of dictionaries
+        combined_dict_list = combined.T.apply(lambda x: x.fillna(value=0).to_dict()).tolist()
+
+        # Update the Pandas NaN fields to None to match Data Warehouse set
+        for dict in combined_dict_list:
+            for k, v in dict.items():
+                if v == 0.0:
+                    dict.update({k: None})
+
+        # Make sets to compare the two data sources
+        dw_people = {(p.forecast_id, p.harvest_id) for p in people_list}
+        combined_set = {(r['fid'], r['hid']) for r in combined_dict_list}
+
+        # Subtract the sets and the remainder is your list to delete
+        delete_set = dw_people - combined_set
+
+        """To finish: get data warehouse pk's from the set above and delete them"""
         pass
