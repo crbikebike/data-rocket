7e3d3286d296a6819eb0fd3fa392dee9bd5a126a
crbikebike <chris@eatingrd.com>
2018-07-22 16:57:07 -0700

    Switched around the sections in classes for easier reading. (#20)

--- controllers/datagrabber.py
+++ controllers/datagrabber.py
@@ -33,118 +33,6 @@ class Harvester(object):
         self.is_test = is_test
 
     """
-    Utility Methods
-    These reduce code duplication and help keep the core methods as clean as possible
-    """
-
-    def __filter_results__(self, results_dict, filter_list):
-        # Filter results from larger dictionary by subtracting a set of its keys against a given list
-        result_keys = set(results_dict.keys())
-        filter_set = set(filter_list)
-        # Compare the two sets, pop all keys that are not in the filter set
-        pop_list = list(result_keys - filter_set)
-
-        for pop_key in pop_list:
-            results_dict.pop(pop_key)
-
-        return results_dict
-
-
-    def __flatten_results__(self, results_dict):
-        """
-        Cycle through a dict result with sub-dicts and return a flattened dict
-        The fields we care about all have, id, name, and code as sub-keys
-        """
-        filter_list = ['id', 'name', 'code']
-        # Copy dict so we can write/delete without affecting original while iterating
-        flattened_dict = results_dict.copy()
-
-        # Find each parent key:value that is a dict and flatten
-        for pk, pv in results_dict.items():
-            if type(pv) is dict:
-                pv = self.__filter_results__(results_dict=pv, filter_list=filter_list)
-                flattened_fields = {'{pk}_{sk}'.format(pk=pk, sk=sk): sv
-                                    for sk, sv in pv.items()}
-                flattened_dict.update(flattened_fields)
-                flattened_dict.pop(pk)
-            elif type(pv) is list:
-
-                json_pv = json.dumps(pv)
-                flattened_dict.update(roles=json_pv)
-            else:
-                # If the value isn't a dict, do nothing
-                pass
-
-        return flattened_dict
-
-    def __get_request__(self, api_url, extra_params=''):
-        """
-        Will hit a harvest API and return result as json object - Called for each paginated result in Harvest API
-        """
-        full_url = self.harvest_base_url + api_url
-        headers = self.harvest_headers
-        params = self.harvest_params
-        params.update(extra_params)
-
-        try:
-            r = requests.get(url=full_url, headers=headers, params=params)
-            json_r = json.loads(r.text)
-        except Exception as e:
-            json_r = {}
-            print('Could not hit endpoint {ep} because {e}'.format(ep=api_url, e=e))
-
-        return json_r
-
-    def __get_api_data__(self, root_key, extra_params='', filters=''):
-        """
-        Accepts params for Harvest endpoint and returns result set. Meant to be portable for all v2 endpoints.
-        """
-        api_params = {}
-        api_params.update(extra_params)
-        api_json_result = self.__get_request__(api_url=root_key, extra_params=api_params)
-
-        # Get page numbers, build queue
-        page_qty = api_json_result['total_pages']
-        page_queue = deque(range(1, (page_qty + 1)))
-        total_pages = api_json_result['total_pages']
-        total_entries = api_json_result['total_entries']
-
-        # Process the queue until empty
-        api_list = []
-        id_list = [] # Keep track of ids added to list to prevent inserting multiple of the same record
-        api_json_result.update(id_list=id_list)
-
-        print('Starting {name} Harvest Pull ({entries} Entries, {pages} Pages)'.format(name=root_key.capitalize(),
-                                                                                       entries=total_entries,
-                                                                                       pages=total_pages))
-        while len(page_queue) > 0:
-            page_num = page_queue.popleft()
-            api_params.update(page=page_num)
-            # Request api load for the current page
-            page_json_result = self.__get_request__(api_url=root_key, extra_params=api_params)
-            api_entities = page_json_result[root_key]
-            # print(
-            #     'Processing Page: ' + str(page_json_result['page']) + ' out of ' + str(page_json_result['total_pages']))
-
-            # If there are keys to filter, do that. Otherwise just add the entire resposne to the api_list
-            for entity in api_entities:
-                if entity['id'] not in api_json_result['id_list']:
-                    entity = self.__filter_results__(results_dict=entity, filter_list=filters)
-                    # Some results have sub-dictionaries so we want to flatten them
-                    flat_entity = self.__flatten_results__(entity)
-                    api_list.append(flat_entity)
-                    id_list.append(flat_entity['id'])
-                else:
-                    pass
-
-            logger.print_progress_bar(iteration=page_json_result['page'], total=total_pages)
-
-        # Replace the endpoint data with our updated info
-        api_json_result.update({root_key: api_list})
-
-        return api_json_result
-
-    """
     All methods below start with get_ correspond to a Harvest v2 API endpoint by the same name as the root_key var
     Each has a filter list that cuts the number of fields down to what is important for the data warehouse
     """
@@ -207,6 +95,125 @@ class Harvester(object):
         tasks_dict = self.__get_api_data__(root_key=root_key, filters=filters, extra_params=task_params)
         return tasks_dict
 
+    """
+    Utility Methods
+    These reduce code duplication and help keep the core methods as clean as possible
+    """
+
+    def __filter_results__(self, results_dict, filter_list):
+        # Filter results from larger dictionary by subtracting a set of its keys against a given list
+        result_keys = set(results_dict.keys())
+        # If the filter list being passed is the default None type, then set it as an empty set
+        if filter_list:
+            filter_set = set(filter_list)
+        else:
+            filter_set = set()
+        # Compare the two sets, pop all keys that are not in the filter set
+        pop_list = list(result_keys - filter_set)
+
+        for pop_key in pop_list:
+            results_dict.pop(pop_key)
+
+        return results_dict
+
+
+    def __flatten_results__(self, results_dict):
+        """
+        Cycle through a dict result with sub-dicts and return a flattened dict
+        The fields we care about all have, id, name, and code as sub-keys
+        """
+        filter_list = ['id', 'name', 'code']
+        # Copy dict so we can write/delete without affecting original while iterating
+        flattened_dict = results_dict.copy()
+
+        # Find each parent key:value that is a dict and flatten
+        for pk, pv in results_dict.items():
+            if type(pv) is dict:
+                pv = self.__filter_results__(results_dict=pv, filter_list=filter_list)
+                flattened_fields = {'{pk}_{sk}'.format(pk=pk, sk=sk): sv
+                                    for sk, sv in pv.items()}
+                flattened_dict.update(flattened_fields)
+                flattened_dict.pop(pk)
+            elif type(pv) is list:
+
+                json_pv = json.dumps(pv)
+                flattened_dict.update(roles=json_pv)
+            else:
+                # If the value isn't a dict, do nothing
+                pass
+
+        return flattened_dict
+
+    def __get_request__(self, api_url, extra_params=None):
+        """
+        Will hit a harvest API and return result as json object - Called for each paginated result in Harvest API
+        """
+        full_url = self.harvest_base_url + api_url
+        headers = self.harvest_headers
+        params = self.harvest_params
+        if extra_params:
+            params.update(extra_params)
+
+        try:
+            r = requests.get(url=full_url, headers=headers, params=params)
+            json_r = json.loads(r.text)
+        except Exception as e:
+            json_r = {}
+            print('Could not hit endpoint {ep} because {e}'.format(ep=api_url, e=e))
+
+        return json_r
+
+    def __get_api_data__(self, root_key, extra_params=None, filters=None):
+        """
+        Accepts params for Harvest endpoint and returns result set. Meant to be portable for all v2 endpoints.
+        """
+        api_params = {}
+        if extra_params:
+            api_params.update(extra_params)
+        api_json_result = self.__get_request__(api_url=root_key, extra_params=api_params)
+
+        # Get page numbers, build queue
+        page_qty = api_json_result['total_pages']
+        page_queue = deque(range(1, (page_qty + 1)))
+        total_pages = api_json_result['total_pages']
+        total_entries = api_json_result['total_entries']
+
+        # Process the queue until empty
+        api_list = []
+        # Keep track of ids added to list to prevent inserting multiple of the same record
+        id_list = []
+        api_json_result.update(id_list=id_list)
+
+        print('Starting {name} Harvest Pull ({entries} Entries, {pages} Pages)'.format(name=root_key.capitalize(),
+                                                                                       entries=total_entries,
+                                                                                       pages=total_pages))
+        while len(page_queue) > 0:
+            page_num = page_queue.popleft()
+            api_params.update(page=page_num)
+            # Request api load for the current page
+            page_json_result = self.__get_request__(api_url=root_key, extra_params=api_params)
+            api_entities = page_json_result[root_key]
+            # print(
+            #     'Processing Page: ' + str(page_json_result['page']) + ' out of ' + str(page_json_result['total_pages']))
+
+            # If there are keys to filter, do that. Otherwise just add the entire resposne to the api_list
+            for entity in api_entities:
+                if entity['id'] not in api_json_result['id_list']:
+                    entity = self.__filter_results__(results_dict=entity, filter_list=filters)
+                    # Some results have sub-dictionaries so we want to flatten them
+                    flat_entity = self.__flatten_results__(entity)
+                    api_list.append(flat_entity)
+                    id_list.append(flat_entity['id'])
+                else:
+                    pass
+
+            logger.print_progress_bar(iteration=page_json_result['page'], total=total_pages)
+
+        # Replace the endpoint data with our updated info
+        api_json_result.update({root_key: api_list})
+
+        return api_json_result
+
 
 class Forecaster(object):
     # Grabs Forecast data - Note that the Forecast API is not officially supported
@@ -220,67 +227,7 @@ class Forecaster(object):
         self.is_test = is_test
 
     """
-    Utility Methods
-    These reduce code duplication and help keep the core methods as clean as possible
-    """
-
-    def __get_api_data__(self, api_url, extra_params=''):
-        """
-        Hits the Forecast endpoint specified by the api_url param
-        """
-        full_url = self.forecast_base_url + api_url
-        headers = self.forecast_headers
-        params = self.forecast_params
-        params.update(extra_params)
-
-        # Perform api request
-        try:
-            r = requests.get(url=full_url, headers=headers, params=params)
-            api_json_result = json.loads(r.text)
-        except Exception as e:
-            print('Attempt for Forecast {url} failed because {e}'.format(url=api_url, e=e))
-
-        return api_json_result
-
-    def __filter_results__(self, results_dict, filter_list):
-        """
-        Filter results from larger dictionary by subtracting a set of its keys against a given list
-        """
-        result_keys = set(results_dict.keys())
-        filter_set = set(filter_list)
-
-        # Compare the two sets, pop all keys that are not in the filter set
-        pop_list = list(result_keys - filter_set)
-        for pop_key in pop_list:
-            results_dict.pop(pop_key)
-
-        return results_dict
-
-    def __get_forecast_dates__(self):
-        """
-        Gets the first date of the previous month and last day of 3 months from now
-        This is used to see how much Forecast history and future to pull
-        """
-        if self.is_test:
-            past_dates = 2
-            future_dates = 3
-        else:
-            past_dates = 180
-            future_dates = 180
-
-        now = datetime.now()
-        # Get the first date of the past month
-        time_machine = now - timedelta(days=past_dates)
-        look_behind = time_machine.strftime('%Y-%m-01')
-        # Get the last date of 6-ish months from now
-        future_machine = now + timedelta(days=future_dates)
-        eom = calendar.monthrange(future_machine.year, future_machine.month)[1]
-        look_ahead = future_machine.strftime('%Y-%m-{d}'.format(d=eom))
-
-        return look_behind, look_ahead
-
-    """
-    The below fuctions pull data from the Forecast API
+    The below functions pull data from the Forecast API
     There is also a set of filters for each endpoint to limit fields being sent downstream
     """
 
@@ -334,3 +281,67 @@ class Forecaster(object):
             client = self.__filter_results__(results_dict=client, filter_list=filters)
 
         return client_json_result
+
+    """
+    Utility Methods
+    These reduce code duplication and help keep the core methods as clean as possible
+    """
+
+    def __get_api_data__(self, api_url, extra_params=None):
+        """
+        Hits the Forecast endpoint specified by the api_url param
+        """
+        full_url = self.forecast_base_url + api_url
+        headers = self.forecast_headers
+        params = self.forecast_params
+        if extra_params:
+            params.update(extra_params)
+
+        # Perform api request
+        try:
+            r = requests.get(url=full_url, headers=headers, params=params)
+            api_json_result = json.loads(r.text)
+        except Exception as e:
+            print('Attempt for Forecast {url} failed because {e}'.format(url=api_url, e=e))
+
+        return api_json_result
+
+    def __filter_results__(self, results_dict, filter_list):
+        """
+        Filter results from larger dictionary by subtracting a set of its keys against a given list
+        """
+        result_keys = set(results_dict.keys())
+        if filter_list:
+            filter_set = set(filter_list)
+        else:
+            filter_set = set()
+
+        # Compare the two sets, pop all keys that are not in the filter set
+        pop_list = list(result_keys - filter_set)
+        for pop_key in pop_list:
+            results_dict.pop(pop_key)
+
+        return results_dict
+
+    def __get_forecast_dates__(self):
+        """
+        Gets the first date of the previous month and last day of 3 months from now
+        This is used to see how much Forecast history and future to pull
+        """
+        if self.is_test:
+            past_dates = 2
+            future_dates = 3
+        else:
+            past_dates = 180
+            future_dates = 180
+
+        now = datetime.now()
+        # Get the first date of the past month
+        time_machine = now - timedelta(days=past_dates)
+        look_behind = time_machine.strftime('%Y-%m-01')
+        # Get the last date of 6-ish months from now
+        future_machine = now + timedelta(days=future_dates)
+        eom = calendar.monthrange(future_machine.year, future_machine.month)[1]
+        look_ahead = future_machine.strftime('%Y-%m-{d}'.format(d=eom))
+
+        return look_behind, look_ahead
